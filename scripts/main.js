9926c88559e2cdd06c91133fc65d9ee12cb0e2ed
Ignacio Kruchowski <ignakruchowski@gmail.com>
2022-06-01 19:32:20 -0300

    Defining styles

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,9 @@ display.value = counter;
 const increaseCounter = () => {
     counter++;
     display.value = counter;
+    if (counter > 0) {
+        display.style.color = "green";
+    }
 }
 
 const decreaseCounter = () => {
@@ -12,12 +15,17 @@ const decreaseCounter = () => {
         counter--;
         display.value = counter;
     }
+    if (counter === 0) {
+        display.style.color = "black"
+    }
 }
 
 const resetCounter = () => {
     counter = 0;
     display.value = counter;
+    display.style.color = "black"
 }
+
 
 btnIncrease.addEventListener("click",increaseCounter);
 btnDecrease.addEventListener("click",decreaseCounter);
